e9cdaa24a03e1e0fce9a959f0a0b5944efabbc88
Asma Shahid <asmashahid795@gmail.com>
2024-02-29 19:31:54 +0500

    Add files via upload
    trim & stripped a person name

---
+++ ex6.js
@@ -0,0 +1,4 @@
+var personName = "\n\t Asma Shahid\t\n";
+console.log(personName);
+var stripped = personName.trim();
+console.log(stripped);
---
+++ ex6.ts
@@ -0,0 +1,4 @@
+let personName: string = `\n\t Asma Shahid\t\n`;
+console.log(personName);
+let stripped: string = personName.trim();
+console.log(stripped);
